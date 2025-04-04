b352ab65354ad48e9bb9c582cda4b67efe078c13
unknown <we.mihir@gmail.com>
2025-04-04 10:56:39 -0400

    trying waitress

--- backend/app.py
+++ backend/app.py
@@ -49,5 +49,5 @@ if __name__ == '__main__':
         serve(app, host='0.0.0.0', port=port)
     else:
         #app.run(debug=True)  # 
-        serve(app, host='0.0.0.0', port=5000, debug=True)
+        serve(app, host='0.0.0.0', port=5000)
     #app.run(debug=True)
